7e2065e932ab23fbf13d6c9ce936b9e07cd1450c
ShawnH1 <ShawnHawkey86@gmail.com>
2021-09-12 14:21:30 -0400

    password generator coded but not working properly, needs refactoring

--- assets/script.js
+++ assets/script.js
@@ -1,77 +1,80 @@
-// Assignment code here
-var enter;
-var confirmUpperCase;
-var confirmLowerCase;
-var confirmNumber;
-var confirmSpecialCharacters;
+var passwordText = document.querySelector("#password");
 
-//Is there an easier way of generating a random letter.
-//Edit: Might change it to a string that has all of the letters and then pull a random character from that array
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//A list of all of different possible options for characters to be added into the password.
+var specialArray = ["!", "@","$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "="];
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//I just used the symbols above the top numbers on the keyboard.
-specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+//passwordArray currently left empty as it will be filled with the different characters after the quantity is selected.
+var passwordArray = [];
 
-//number generator
-//This should generate a random number between 1 and 10.
-//It does this by picking a number between 0 and 0.9999...
-// Then it multiplies that number by 10.
-//example 0.12 would become 1.2 and 0.92 would become 9.2.
-//Then Math.floor gets rid of the integer.
-//1.2 would become 1 and 9.2 would become 9 
-number = let= Math.floor(Math.random() * 10);
-
-// converts letters to uppercase
-var toUpper = function (x) {
-  return x.toUpperCase():
-};
-
-//variable for user inpute
-var choices;
-// Get references to the #generate element
-var make = document.querySelector("#generate");
-
-var copy = document.querySelector("#copy");
-copy.addEventListener("click", function () {
-  copyPass();
-}
-
-//When user clicks "Generate"
-function generatePassword() {
-  enter = parseInt(window.prompt("Please pick how long you want the password.  It can be between 8 and 128 characters."));
-  //Did they actually type anything?
-  if (!enter) {
-    alert("Please type a valid input, a number between 8 and 128!")
-  } else if (enter < 8 || enter > 128) {
-    //Did they put in a valid response?  If so, continue to ask them for more details about their password
-    enter = parseInt(prompt("Please type a valid input, a number between 8 and 128!"))
-  }else {
-    confirmUpperCase = window.confirm("Do you want the password to contain uppercase letters?");
-    confirmLowerCase = window.confirm("Do you want the password to contain lowercase letters as well?");
-    confirmSpecialCharacters = window.confirm("Do you want special characters as well such as !@# ?");
-    confirmNumber = window.confirm("Do you want this to contain numbers?")
+function generatePassword(){
+  var userInput2 = parseInt( prompt("Please pick a length for your password between 8 and 126 characters long"));
+  if (isNan(userInput2)) {
+    alert("Please enter a number between 8 and 126!");
+    return
   }
 }
 
+//Asking the user for the parameters of their password
+var specialCharacters = confirm("Would you like special characters like '!', '@', and '#' to be included? in your password?");
+var numericValues = confirm("Would you like numbers to be included in your password?");
+var lowerCaseValues = confirm("Would you like lower case letters in your password?");
+var upperCaseValues = confirm("Woul you like upper case letters in your password?");
 
-
-// Write password to the #password input
-function (UserInput(pass).text
-  document.getElementById("password").textContent = ps
+//If the user rejected all possibilities for characters in the password, inform them of the issue and return them to the list of options.
+if (!specialCharacters && !numericValues && !lowerCaseValues && !upperCaseValues) {
+  alert ("Unable to make a password without any conditions.  Please pick at least one condition.  You will now be returned to the list of options for conditions.");
+  return
 }
 
-// Add event listener to generate button
-get.addEventListener("click", function () {
-  pass = generatePassword();
-  document.getElementbyId(".password").placeholder = password;
+//check for userInput2, useful for debugging in the future if there are issues.
+console.log(userInput2)
 
-};
+//Puts the character options the user has selected into the password array
+//This one is for special characters such as !@#
+if (specialCharacters){
+  for (var i = 0; i < specialArray.length; i++){
+    const element = speicalArray[i]
+    passwordArray[passwordArray.length] = element
+  }
+}
 
+//This one is for numbers such as 123...
+if (numericValues){
+  for (var i = 0; i < numericValues.length; i++){
+    const element = numericValues[i]
+    passwordArray[passwordArray.length] = element
+  }
+}
 
+//this one is for lower case letters such as abc...
+if (lowerCaseValues){
+  for (var i = 0; i < lowerCaseValues.length; i++){
+    const element = lowerCaseValues[i]
+    passwordArray[passwordArray.length] = element
+  }
+}
 
-//provides "copy" functionality
-function copyPass() {
-  document.getElementbyId(".password").select();
-  document.execCommand("Copy");
-  window.alert("Your password has been copied!")
+//this one is for upper case letters such as ABC...
+if (upperCaseValues){
+  for (var i = 0; i < upperCaseValues.length; i++){
+    const element = upperCaseValues[i]
+    passwordArray[passwordArray.length] = element
+  }
+}
+
+//temporary holding space for the password characters to be held
+var passPhrase = "";
+
+//Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+//This puts the password in the #password space
+function writePassword(){
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
