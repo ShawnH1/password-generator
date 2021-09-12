76596c83275e419e236ff03cdc7ab06ba2a2ce7a
ShawnH1 <ShawnHawkey86@gmail.com>
2021-09-12 15:38:31 -0400

    added and clarified comments to clarify code for future developers and edited the README.md

--- assets/script.js
+++ assets/script.js
@@ -12,17 +12,19 @@ var passwordArray = [];
 
 function generatePassword(){
   var userInput2
+
+  //This asks the user to put in a number to determine the length of their password.
   var userInput2 = parseInt( prompt("Please pick a length for your password between 8 and 126 characters long"));
 
-  
+  //This checks if the user input was not a number.
   if (isNaN (userInput2)) {
-    alert("Please enter a number between 8 and 126!");
+    alert("We apologize for the inconvenience but this generator requires a number between 8 and 126.  You will now be returned that choice.  Please type in a number in that range.");
     return
   }
 
-
+//This makes sure that the number selected by the user is in the range of 8 and 126.
 if (userInput2 < 8 || userInput2 > 126) {
-  alert("Please input a value between 8 and 126.");
+  alert("We apologize for the inconvenience but this generator can only make passwords between 8 and 126 characters.  You will now be returned to that option.  Please input a value between 8 and 126.");
   return
 }
 
@@ -34,7 +36,7 @@ var uppercaseValues = confirm("Woul you like upper case letters in your password
 
 //If the user rejected all possibilities for characters in the password, inform them of the issue and return them to the list of options.
 if (!specialCharacters && !numericValues && !lowercaseValues && !uppercaseValues) {
-  alert ("Please pick at least one condition.  You will now be returned to the list of options for conditions.");
+  alert ("We apologize for the inconvenience but it appears you did not select any options for you password. You will now be returned to the list of options for conditions now.  Please pick at least one.  Thank you for your understanding and have a nice day!");
   return
 }
 
@@ -58,7 +60,7 @@ if (numericValues){
   }
 }
 
-//this one is for lower case letters such as abc...
+//This one is for lower case letters such as abc...
 if (lowercaseValues){
   for (var i = 0; i < lowercaseValues.length; i++){
     const element = lowercaseValues[i]
@@ -66,7 +68,7 @@ if (lowercaseValues){
   }
 }
 
-//this one is for upper case letters such as ABC...
+//This one is for upper case letters such as ABC...
 if (uppercaseValues){
   for (var i = 0; i < uppercaseValues.length; i++){
     const element = uppercaseValues[i]
@@ -75,6 +77,8 @@ if (uppercaseValues){
 }
   var passPhrase = "";
   
+  //This keeps randomly selecting random characters from those available until the password
+  // reaches the length selected by the user (userInput2) which they were asked for at the beginning.
   for (let i = 0; i < userInput2; i++) {
     var finalPassword = passwordArray[Math.floor(Math.random()* passwordArray.length)]
       passPhrase += finalPassword
@@ -96,5 +100,6 @@ function writePassword(){
   passwordText.value = password;
 }
 
-//Add event listener to generate button
+//Add event listener to generate button.  This is VERY important!  Do not break it or nothing else will work!
+//I repeat "Do NOT break this button!"
 generateBtn.addEventListener("click", writePassword);
